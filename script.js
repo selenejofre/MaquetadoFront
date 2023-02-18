4d4de2685d45cdc1cd983048cdc8667e1e8642f7
selene jofre <mselenejofre@gmail.com>
2023-02-18 05:23:41 -0300

    añadiendo estilos con bootstrap

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const switchButton = document.getElementById("switch");
-switchButton.addEventListener("click",()=>{
+switchButton.addEventListener("click", switchToDarkmode)
+
+function switchToDarkmode (){
     document.body.classList.toggle("dark");
     switchButton.classList.toggle("active")
-})
+}
+
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
