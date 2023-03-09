b65b0a4dcf905950b2b927dbeb85a223b1568133
selene jofre <mselenejofre@gmail.com>
2023-03-09 12:42:49 -0300

    añadiendo cambios

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function switchToDarkmode (){
 $(document).ready(function() {
     $(".dropdown-toggle").dropdown();
 });
+
+//Seleccionar boton editar
+const botonEditar = document.getElementById("botonEditar");
+botonEditar.addEventListener("click", editarContenido)
+
+//Editar Nombre y contenido de perfil
+const nombre = document.getElementById("nombre")
+const descripcionDePerfil = document.getElementById("descripcionDePerfil")
